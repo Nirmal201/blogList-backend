56438dfd36a65a790b49af00d10dad534f70451f
Nirmal201 <ninu201@gmail.com>
2020-06-02 01:42:45 -0400

    more test

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,25 +1,39 @@
 const blogRouter = require("express").Router();
 const Blog = require("../models/blog");
 
-blogRouter.get("/", (request, response) => {
+blogRouter.get("/", async (request, response) => {
   Blog.find({}).then((blogs) => {
     response.json(blogs.map((blog) => blog.toJSON()));
   });
 });
-blogRouter.post("/", (request, response) => {
+
+blogRouter.get("/:id", async (request, response) => {
+  const blog = await Blog.findById(request.params.id);
+  if (blog) {
+    response.json(blog.toJSON());
+  } else {
+    response.status(404).end();
+  }
+});
+
+// posting route
+blogRouter.post("/", async (request, response) => {
   const body = request.body;
-  console.log(body);
 
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes,
+    likes: body.likes === undefined ? 0 : body.likes,
   });
 
-  blog.save().then((savedBlog) => {
-    response.json(savedBlog.toJSON());
-  });
+  const savedBlog = await blog.save();
+  response.json(savedBlog.toJSON());
+});
+
+blogRouter.delete("/:id", async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id);
+  response.status(204).end();
 });
 
 module.exports = blogRouter;
